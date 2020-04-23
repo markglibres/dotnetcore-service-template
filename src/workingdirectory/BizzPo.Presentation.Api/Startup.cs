3192f9325b82602616bd13c7cddc294596647408
Mark Gil Libres <markglibres@gmail.com>
2020-04-23 21:54:37 +1000

    add template config

--- src/workingdirectory/BizzPo.Presentation.Api/Startup.cs
+++ src/workingdirectory/BizzPo.Presentation.Api/Startup.cs
@@ -1,18 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using BizzPo.Presentation.Common;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 
-namespace BizzPo.Api
+namespace BizzPo.Presentation.Api
 {
     public class Startup
     {
@@ -37,9 +30,11 @@ namespace BizzPo.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
-
+            else
+            {
+                app.UseHttpsRedirection();
+            }
+            
             app.UseRouting();
 
             app.UseAuthorization();
